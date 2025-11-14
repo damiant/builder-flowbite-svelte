e73aa64dbc94fb192f15698f892fac1ca202552f
Damian Tarnawsky <damian@builder.io>
2025-11-14 13:12:45 -0800

    fix: center

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 </script>
 
-<div class="flex items-center justify-center min-h-screen">
-  <h1 class="text-4xl font-bold">Flowbite Svelte Starter</h1>
+<div class="flex items-center justify-center min-w-screen min-h-screen">
+  <h1 class="text-4xl font-bold text-center">Flowbite Svelte Starter</h1>
 </div>
