d52a119c697de6f137e0417ef53d04318121b3cf
Damian Tarnawsky <damian@builder.io>
2025-11-14 08:57:59 -0800

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+
+<script>
+	import 'flowbite';
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div class="flex items-center justify-center min-h-screen">
+  <h1 class="text-4xl font-bold">Flowbite Svelte Starter</h1>
+</div>
